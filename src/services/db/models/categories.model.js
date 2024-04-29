d40d6c8954220b84a3a9888fb5daa9e4270935f1
Amneris Calle <amneris.calle@gmail.com>
2024-04-28 21:38:32 -0300

    moviendo archivos y carpetas

--- src/services/db/models/categories.model.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-const CATEGORIES_COLLECTION = "categories";
-
-const categorySchema = new mongoose.Schema({
-  category: { type: String, required: true, unique: true },
-});
-const categoriesModel = mongoose.model(CATEGORIES_COLLECTION, categorySchema);
-
-export default categoriesModel;
