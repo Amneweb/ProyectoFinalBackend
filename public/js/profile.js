d5037ca9ba112c060c4b8034bb4806fc0f58ae09
Amneris Calle <amneris.calle@gmail.com>
2024-05-05 09:46:00 -0300

    peleando con las dos promesas para armar el profile

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,36 +1,36 @@
 function llamarApi() {
   console.log("Llamando api users.");
 
-  fetch("api/users/currentUser", {
+  const promise1 = fetch("api/users/currentUser", {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
       Authorization: `Bearer ${localStorage.getItem("authToken")}`,
     },
-  }).then((result) => {
+  });
+  //me fijo si el usuario tiene un carrito armado
+  const promise2 = fetch("api/users/email/");
+  Promise.all([promise1, promise2]).then((results) => {
+    console.log("resultados de promesas");
+    console.log("0", results[0]);
+    console.log("1", results[1]);
+    results[0].json().then((json) => {
+      console.log("0", json);
+    });
+    results[1].json().then((json) => console.log("1", json));
+  });
+
+  /*.then((json) => {
+  if (json.userCartID.length > 0) {
+    plantilla.innerHTML += `<p><strong>Carrito guardado:</strong> ${json.userCartID[0]}</p>`;
+  }
+});
+});
+
+de la primera parte 
+.then((result) => {
     if (result.status === 200) {
-      result
-        .json()
-        .then((json) => {
-          console.log("resultado en llamando api", json);
-          const plantilla = document.getElementById("info");
-          plantilla.innerHTML = `<p><strong>Nombre:</strong> ${json.payload.name}</p>
-        <p><strong>Email:</strong> ${json.payload.email}</p>
-        <p><strong>Edad:</strong> ${json.payload.age}</p>
-        <p><strong>Rol:</strong> ${json.payload.role}</p>`;
-        })
-        .then((json) => {
-          //me fijo si el usuario tiene un carrito armado
-          fetch("api/users/email/").then((result) => {
-            console.log("dentro del fetch de email");
-            console.log(result);
-            result.json().then((json) => {
-              if (json.userCartID.length > 0) {
-                plantilla.innerHTML += `<p><strong>Carrito guardado:</strong> ${json.userCartID[0]}</p>`;
-              }
-            });
-          });
-        });
+      result.json();
     } else if (result.status === 401) {
       console.log(result);
       alert("Credenciales invalidas, debes loguearte de nuevo.");
@@ -40,7 +40,8 @@ function llamarApi() {
       alert("Usuario no autorizado, revisa tus accesos.");
       window.location.replace("/users/login");
     }
-  });
+
+*/
 }
 llamarApi();
 const deslogueo = document.querySelector("#logout");
