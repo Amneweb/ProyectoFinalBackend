9c9902d03bba24760a7db972b59a505e3e505491
Amneris Calle <amneris.calle@gmail.com>
2024-07-13 14:55:26 -0300

    pasando los helpers de hb a un archivo separado

---
+++ src/utils/hb-helpers.js
@@ -0,0 +1,31 @@
+export function formatear(amount) {
+  const formateado = new Intl.NumberFormat("es-AR", {
+    style: "currency",
+    currency: "ARS",
+  }).format(amount);
+  return formateado;
+}
+
+export function comparar(first, operation, second) {
+  if (eval(first + operation + second)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+export function operacion(first, second, operacion) {
+  let resultado;
+  switch (operacion) {
+    case "producto":
+      resultado = first * second;
+      break;
+    case "suma":
+      resultado = first + second;
+      break;
+    case "resta":
+      resultado = first - second;
+
+      break;
+  }
+  return resultado;
+}
